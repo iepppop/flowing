80a4993b608b6343d34948f4a48699cf355c1ee3
iepppop <iepppop@gmail.com>
2022-08-10 18:01:54 +0900

    m

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,28 +6,55 @@ import {
   useTransform,
   MotionValue
 } from "framer-motion";
-import AboutTitle from './AboutTitle'
 
 const About = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  });
-
   return (
     <Container>
-      <AboutTitle />
+      <ImgWrap>
+      </ImgWrap>
+      <TextWrap>
+        <ul>
+          <li>I like natural traces.</li>
+          <li>Clothes,</li>
+          <li>furniture,</li>
+          <li>and lighting The time,</li>
+          <li>design.</li>
+        </ul>
+      </TextWrap>
     </Container>
   )
 }
 export default About;
 
 const Container = styled.div`
-    display:flex;
-    width:calc(100vw - 20vw);
-    margin:0 auto;
-    position:relative;
+  display:flex;
+  width:calc(100vw - 20vw);
+  margin:0 auto;
+  position:relative;
+  height:100vh;
+  align-items: center;
 `
+const ImgWrap = styled.div`
+  width:50%;
+  height:100%;
+`
+
+const TextWrap = styled.div`
+  width:50%;
+  height:100%;
+  position:relative;
+
+  ul{
+    position:sticky;
+    top:50%;
+    transform:translate(0,-50%);
+  }
+
+  li{
+    font-size: clamp(15px, 10vw, 4rem);
+    line-height:140%;
+    font-weight:500;
+  }
+`
+
 
